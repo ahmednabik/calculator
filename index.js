a6eaceccdecb2e9e69e71ac7ab1cdc6eebe08de6
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-07-02 15:21:22 +0500

    write basic javascript functions

---
+++ index.js
@@ -0,0 +1,19 @@
+function add (a,b) {
+    return a + b;
+}
+
+function minus (a,b) {
+    return a - b;
+}
+
+function multiply (a,b) {
+    return a * b;
+}
+
+function divide (a,b) {
+    return a / b;
+}
+
+function percent (a,b) {
+    return a % b;
+}
