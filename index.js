721b2eeb1fbfc2d3913f633f4135ae05fbb84213
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-07-03 18:27:26 +0500

    coded part of basic logic

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+let currentEquation = '';
+
+let calculatorKeys = Array.from (document.getElementsByClassName("display-input"));
+
+calculatorKeys.forEach(div => { div.addEventListener ('click', showEquationDisplay)
+    
+});
+
+function showEquationDisplay (e) {
+    let equationDisplay = document.getElementById ('equation-display');
+    equationDisplay.innerText = equationDisplay.innerHTML + " " + e.target.innerHTML;
+    currentEquation = equationDisplay.innerText;
+}
+
+
+
+function calculate (number1, number2, operator) {
+    switch (operator) {
+        case "+" :
+            return add (number1,number2)
+            break;
+        case "-":
+            return minus (number1,number2)
+            break;
+        case "x":
+            return multiply (number1,number2)
+            break;
+        case "/":
+            return divide (number1,number2)
+            break;
+        case "%":
+            return percent (number1,number2)
+            break;
+        default:
+            break;
+    }
+}
+
 function add (a,b) {
     return a + b;
 }
